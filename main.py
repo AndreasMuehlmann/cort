2db05b6150f730673bd4142159fdcdf8e05cb142
andi <andi.muehlmann@arcor.de>
2021-12-11 17:14:41 +0100

    works with multithreading

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+
 from collections import deque
 from dataclasses import dataclass
-from multiprocessing import Process, Queue
+from threading import Thread
+import sys
 import time
 import winsound
 
-#TODO: use PyQt
-
 #time in seconds
-TIME_TEST = 1 * 10
-SLEEP_TIME = 3
+TIME_TEST = 1 * 5
 
 @dataclass
 class Person:
     name : str
     time : int 
 
+class MyThread (Thread) :
+    def __init__(self, queue, method, pipe):
+        Thread.__init__(self)
+        self.queue = queue
+        self.method = method
+        self.pipe = pipe
+
+    def run(self):
+        self.method(self.queue, self.pipe)
+
 def make_sound(person):
     print(f'for {person.name} the time is up') 
     winsound.Beep(440, 500)
     winsound.Beep(490, 500)
     winsound.Beep(300, 500)
-    winsound.Beep(440, 500)
+    winsound.Beep(440, 500) 
 
 def print_queue(queue):
     print('\npersons in queue:\n')
@@ -28,21 +37,38 @@ def print_queue(queue):
         print(f'{person.name}: {(int(((TIME_TEST - (time.time() - person.time)) / 60) * 10)) / 10}min')
     print()
 
-def main():
-    queue = deque()
-    queue.append(Person(input(), time.time()))
-
-    while queue:
-        if time.time() - queue[0].time >= TIME_TEST:
-            make_sound(queue.pop())
-
+def get_input(queue, pipe):
+    while True:
         message = input()
 
         if message:
             if message == 'show':
                 print_queue(queue)
+
+            elif message == 'exit':
+                pipe.append('exit')
+                sys.exit(0)
+
             else:
                 queue.append(Person(message, time.time()))
+
+def check_ready(queue, pipe):
+    while True:
+        if queue and time.time() - queue[0].time >= TIME_TEST:
+            make_sound(queue.popleft())
+
+        if pipe and pipe[0] == 'exit':
+            sys.exit(0)
+
+def main():
+    queue = deque()
+    pipe = []
+
+    t1 = MyThread(queue, get_input, pipe)
+    t2 = MyThread(queue, check_ready, pipe)
+
+    t1.start()
+    t2.start()
 
 if __name__ == '__main__':
     main()
